b942ac2eb6cf801125a74a390d35064b5b7f5528
linning <linningde25@gmail.com>
2025-01-04 03:13:06 +0800

    Add evm tx benchmark
    Signed-off-by: linning <linningde25@gmail.com>

--- frame/ethereum/src/tests/mod.rs
+++ frame/ethereum/src/tests/mod.rs
@@ -28,6 +28,10 @@ use crate::{
 	mock::*, CallOrCreateInfo, Event, RawOrigin, Transaction, TransactionAction, H160, H256, U256,
 };
 use fp_self_contained::CheckedExtrinsic;
+use frame_support::traits::Hooks;
+use pallet_evm::AddressMapping;
+use sp_core::keccak_256;
+use std::time::Instant;
 
 mod eip1559;
 mod eip2930;
@@ -52,3 +56,62 @@ pub const ERC20_CONTRACT_BYTECODE: &str = include_str!("./res/erc20_contract_byt
 // 	   }
 // }
 pub const TEST_CONTRACT_CODE: &str = "608060405234801561001057600080fd5b50610129806100206000396000f3fe6080604052348015600f57600080fd5b506004361060325760003560e01c8063c2985578146037578063febb0f7e146055575b600080fd5b603d605d565b60405180821515815260200191505060405180910390f35b605b6066565b005b60006001905090565b600060bc576040517f08c379a00000000000000000000000000000000000000000000000000000000081526004018080602001828103825260358152602001806100bf6035913960400191505060405180910390fd5b56fe766572795f6c6f6e675f6572726f725f6d73675f746861745f77655f6578706563745f746f5f62655f7472696d6d65645f61776179a26469706673582212207af96dd688d3a3adc999c619e6073d5b6056c72c79ace04a90ea4835a77d179364736f6c634300060c0033";
+
+pub fn address_build(seed_number: u64) -> AccountInfo {
+	let mut seed = [0u8; 32];
+	seed[0..8].copy_from_slice(&seed_number.to_be_bytes());
+	let private_key = H256::from_slice(&seed);
+	let secret_key = libsecp256k1::SecretKey::parse_slice(&private_key[..]).unwrap();
+	let public_key = &libsecp256k1::PublicKey::from_secret_key(&secret_key).serialize()[1..65];
+	let address = H160::from(H256::from(keccak_256(public_key)));
+
+	AccountInfo {
+		private_key,
+		account_id: <Test as pallet_evm::Config>::AddressMapping::into_account_id(address),
+		address,
+	}
+}
+
+#[test]
+fn evm_transact_1000() {
+	n_evm_transact(1000);
+}
+
+#[test]
+fn evm_transact_4000() {
+	n_evm_transact(4000);
+}
+
+#[test]
+fn evm_transact_8000() {
+	n_evm_transact(8000);
+}
+
+fn n_evm_transact(n: u64) {
+	let (acc, mut ext) = new_test_ext_with_initial_balance(1, u64::MAX / 2);
+	ext.execute_with(|| {
+		let sender = &acc[0];
+		let txs: Vec<_> = (1..n)
+			.map(|i| {
+				let receiver = address_build(i);
+				LegacyUnsignedTransaction {
+					nonce: U256::from(i - 1),
+					gas_price: U256::from(10000),
+					gas_limit: U256::from(21000),
+					action: ethereum::TransactionAction::Call(receiver.address),
+					value: U256::from(1234567890),
+					input: vec![],
+				}
+				.sign(&sender.private_key)
+			})
+			.collect();
+
+		let start = Instant::now();
+		for tx in txs {
+			Ethereum::transact(RawOrigin::EthereumTransaction(sender.address).into(), tx).unwrap();
+		}
+		Ethereum::on_finalize(1);
+
+		println!("{} tx took {}ms", n, start.elapsed().as_millis());
+	});
+}
